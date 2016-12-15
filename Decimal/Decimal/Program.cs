f2859b46a472ef9b718582994b36007508334edd
nks0005 <nks0005@naver.com>
2016-12-15 14:07:33 +0900

    Decimal

---
+++ Decimal/Decimal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Decimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.1415926535897932384626433383279f;
+            double b = 3.1415926535897932384626433383279;
+            decimal c = 3.1415926535897932384626433383279m;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
