4e2ae500e0b526f593bfcd2c8df22ae91e16bac1
nks0005 <nks0005@naver.com>
2016-12-15 14:10:47 +0900

    Char

---
+++ Char/Char/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = '안';
+            char b = '녕';
+
+            Console.Write(a);
+            Console.WriteLine(b);
+        }
+    }
+}
