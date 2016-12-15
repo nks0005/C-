4fbe99a6a5e687432ad7fa8f7bd46de96c383866
nks0005 <nks0005@naver.com>
2016-12-15 13:52:08 +0900

    Overflow

---
+++ Overflow/Overflow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a = uint.MaxValue;
+
+            Console.WriteLine(a);
+
+            a = a + 1;
+
+            Console.WriteLine(a);
+        }
+    }
+}
