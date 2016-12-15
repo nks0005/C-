32482c398bdcffc4f6b1f48b7d0ee2566a9407fc
nks0005 <nks0005@naver.com>
2016-12-15 14:01:21 +0900

    FloatingPoint

---
+++ FloatingPoint/FloatingPoint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FloatingPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.14159265358979323846f;
+            Console.WriteLine(a);
+
+            double b = 3.14159265358979323846;
+            Console.WriteLine(b);
+
+        }
+    }
+}
