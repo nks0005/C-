0f1d228ae99eac393306dd63a040ba926df29b54
nks0005 <nks0005@naver.com>
2016-12-15 14:11:51 +0900

    String

---
+++ String/String/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "안녕하세요.";
+
+            Console.WriteLine(a);
+        }
+    }
+}
