379a36993f6fd2376da42dcd3485984e52cbca7a
nks0005 <nks0005@naver.com>
2016-12-15 13:45:41 +0900

    2의 보수법 예제 프로그램

---
+++ SignedUnsigned/SignedUnsigned/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SignedUnsigned
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 255; // 1111 1111
+            sbyte b = (sbyte)a; // 1111 1111
+            
+            // 1111 1111을 2의 보수법 > 0000 0000 > 0000 0001 = 1 => -1
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
