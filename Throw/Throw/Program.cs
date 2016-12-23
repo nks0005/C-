b3220432985ca2f9b86068db11a0023da2c66c8e
nks0005 <nks0005@naver.com>
2016-12-24 02:25:23 +0900

    Exception_try_catch_finally

---
+++ Throw/Throw/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Throw
+{
+    class Program
+    {
+        static void DoSomething(int arg)
+        {
+            if (arg < 10)
+                Console.WriteLine("arg : {0}", arg);
+            else
+                throw new Exception("arg가 10보다 큽니다.");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i < 13; i++)
+                    DoSomething(i);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
