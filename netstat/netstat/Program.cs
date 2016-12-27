f2b89258876acde426d592ba2cf1d1ea3475d9ca
nks0005 <nks0005@naver.com>
2016-12-27 20:56:23 +0900

    netstate

---
+++ netstat/netstat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace netstat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+
+            IPEndPoint[] endPoints = ipProperties.GetActiveTcpListeners();
+            TcpConnectionInformation[] tcpConnections = ipProperties.GetActiveTcpConnections();
+
+            foreach (TcpConnectionInformation info in tcpConnections)
+            {
+                Console.WriteLine("Local : " + info.LocalEndPoint.Address.ToString() + ":" + info.LocalEndPoint.Port.ToString()
+                    + "\nRemote : " + info.RemoteEndPoint.Address.ToString()
+                    + ":" + info.RemoteEndPoint.Port.ToString()
+                    + "\nState : " + info.State.ToString() + "\n\n");
+            }
+        }
+    }
+}
