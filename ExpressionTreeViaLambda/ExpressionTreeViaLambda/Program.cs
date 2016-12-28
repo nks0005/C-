f248995989000af4453303c3c83f72ffbceb0ba9
nks0005 <nks0005@naver.com>
2016-12-28 21:44:10 +0900

    Func,Action,Limbda,

---
+++ ExpressionTreeViaLambda/ExpressionTreeViaLambda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTreeViaLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int, int>> expression =
+                (a, b) => 1 * 2 + (a - b);
+            Func<int, int, int> func = expression.Compile();
+
+            Console.WriteLine("1*2+({0}-{1}) = {2}", 7, 8, func(7, 8));
+        }
+    }
+}
---
+++ Lambda_Ex_1/Lambda_Ex_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lambda_Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 11, 22, 33, 44, 55 };
+
+            foreach(int a in array)
+            {
+                Action action = () => Console.WriteLine(a * a);
+                action.Invoke();
+            }
+        }
+    }
+}
